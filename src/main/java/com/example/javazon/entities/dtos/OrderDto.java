8da0acfbced32807b501805b98c72b9f9f694122
skhaled <elwansara406@gmail.com>
2025-12-06 12:48:05 +0200

    push all updates

---
+++ src/main/java/com/example/javazon/entities/dtos/OrderDto.java
@@ -0,0 +1,75 @@
+package com.example.javazon.entities.dtos;
+import com.example.javazon.enums.OrderStatus;
+import com.example.javazon.enums.PaymentMethod;
+
+import java.util.List;
+
+public class OrderDto {
+
+    private int orderId;
+    private double totalPrice;
+    private OrderStatus status;
+    private PaymentMethod paymentMethod;
+    private String address;
+
+    private int userId;
+    private List<OrderItemDto> orderItems;
+
+    public OrderDto() {}
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public List<OrderItemDto> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItemDto> orderItems) {
+        this.orderItems = orderItems;
+    }
+}
