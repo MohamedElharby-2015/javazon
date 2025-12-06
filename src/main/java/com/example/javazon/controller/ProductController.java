90fae9fd492648a26efa97b62efda1e7f6972492
skhaled <elwansara406@gmail.com>
2025-12-06 14:37:18 +0200

    Enhance Product APIs Naming

--- src/main/java/com/example/javazon/controller/ProductController.java
+++ src/main/java/com/example/javazon/controller/ProductController.java
@@ -1,28 +1,24 @@
 package com.example.javazon.controller;
-import com.example.javazon.entities.Product;
+
 import com.example.javazon.entities.dtos.ProductDto;
 import com.example.javazon.model.PagedResponse;
 import com.example.javazon.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @PostMapping("/addProduct")
+    @PostMapping("/add")
     public ProductDto addProduct(@RequestBody ProductDto productDto) {
         return productService.addProduct(productDto);
     }
 
-    @GetMapping("/getAllProducts")
+    @GetMapping("/all")
     public ResponseEntity<PagedResponse<ProductDto>> getAllProducts(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
@@ -30,17 +26,17 @@ public class ProductController {
         return ResponseEntity.ok(productService.getAllProduct(page, size));
     }
 
-    @GetMapping("/getProductById/{id}")
+    @GetMapping("/{id}")
     public ProductDto getProductById(@PathVariable int id) {
         return productService.getProductById(id);
     }
 
-    @PutMapping("/updateProduct/{id}")
+    @PutMapping("/{id}")
     public ProductDto updateProduct(@PathVariable int id, @RequestBody ProductDto product) {
         return productService.updateProduct(id, product);
     }
 
-    @DeleteMapping("/deleteProduct/{id}")
+    @DeleteMapping("/{id}")
     public String deleteProduct(@PathVariable int id) {
         return  productService.deleteProduct(id);
     }
